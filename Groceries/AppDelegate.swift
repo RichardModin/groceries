2c409361f4ed9f607ae4dc75975918b5997787b9
RichardModin <richard.modin@gmail.com>
2025-04-19 12:25:45 -0400

    Adding theme files for future UI updates

---
+++ Groceries/AppDelegate.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Set global appearance
+        return true
+    }
+}
