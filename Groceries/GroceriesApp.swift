0956d5d6083761ef8f1a3ba84a7b59c4c1bc5ea5
RichardModin <richard.modin@gmail.com>
2025-04-18 23:11:26 -0400

    Initial Commit

---
+++ Groceries/GroceriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceriesApp.swift
+//  Groceries
+//
+//  Created by Richard Modin on 2025-04-18.
+//
+
+import SwiftUI
+
+@main
+struct GroceriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
